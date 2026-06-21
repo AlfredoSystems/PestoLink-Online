a9b3be9d584b40681dcd547bf62047083815aac0
Jacob Williams <jrw4561@gmail.com>
2026-06-21 01:45:11 -0400

    Revert "Update MainActivity.java"
    This reverts commit 7e8df45de4899fc628970626f8a27f8d68945019.

--- android/app/src/main/java/com/alfredosys/pestolink/MainActivity.java
+++ android/app/src/main/java/com/alfredosys/pestolink/MainActivity.java
@@ -1,54 +1,5 @@
 package com.alfredosys.pestolink;
 
-import android.os.Build;
-import android.os.Bundle;
-import android.view.View;
-import android.view.WindowInsets;
-import android.view.WindowInsetsController;
-import android.view.WindowManager;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-            getWindow().getAttributes().layoutInDisplayCutoutMode =
-                WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
-        }
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            getWindow().setDecorFitsSystemWindows(false);
-        }
-
-        // Prevent Capacitor's WebView from padding itself for cutout/system bars
-        ViewCompat.setOnApplyWindowInsetsListener(getBridge().getWebView(),
-            (v, insets) -> WindowInsetsCompat.CONSUMED);
-    }
-
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-        if (hasFocus) hideSystemBars();
-    }
-
-    private void hideSystemBars() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            WindowInsetsController c = getWindow().getInsetsController();
-            if (c != null) {
-                c.hide(WindowInsets.Type.navigationBars() | WindowInsets.Type.statusBars());
-                c.setSystemBarsBehavior(WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
-            }
-        } else {
-            getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_FULLSCREEN |
-                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
-                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
-                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
-                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-            );
-        }
-    }
-}
+public class MainActivity extends BridgeActivity {}
